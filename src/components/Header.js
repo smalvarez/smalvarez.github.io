d1484f741c587686bea1bc429816489e0431c610
Steven Alvarez <smalvarez#gmail.com>
2024-05-31 23:31:30 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Congrats! You can apply for a driver's license!</h1>
+    </header>
+  );
+};
+
+export default Header;
